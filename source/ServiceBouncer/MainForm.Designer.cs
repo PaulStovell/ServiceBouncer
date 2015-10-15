e2400cf1b9dd5abfb6110149cc0d343a7b60beac
Caio Proiete <caio.proiete@gmail.com>
2015-10-14 22:19:49 -0300

    Set the MainForm start position to center screen

--- source/ServiceBouncer/MainForm.Designer.cs
+++ source/ServiceBouncer/MainForm.Designer.cs
@@ -226,6 +226,7 @@
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Service Bouncer";
             this.Load += new System.EventHandler(this.FormLoaded);
             this.toolStripContainer1.ContentPanel.ResumeLayout(false);
