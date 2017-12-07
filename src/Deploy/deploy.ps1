ecbe20f462b03c772153320565659738a164f359
Chris Blyth <blythmeister@gmail.com>
2017-12-07 08:53:13 +0000

    Updates to Octopus deployment script

--- src/Deploy/deploy.ps1
+++ src/Deploy/deploy.ps1
@@ -2,28 +2,30 @@ $scriptDir = Split-Path ((Get-Variable MyInvocation -Scope 0).Value.MyCommand.Pa
 
 if($FrameworkNeeded -eq $null)
 {
-    $FrameworkNeeded = "NET461"
+	$FrameworkNeeded = "NET461"
 }
 
 $ServiceBouncer = Get-Process ServiceBouncer -ErrorAction SilentlyContinue
 if ($ServiceBouncer) {
-    Write-Host "Application Running, Attempting to Stop"
-    $ServiceBouncer | Stop-Process -Force
+	Write-Host "Application Running, Attempting to Stop"
+	$ServiceBouncer | Stop-Process -Force
+	Write-Host "Sleeping for 10 seconds to allow for Stop"
+	Start-Sleep -s 10
 }
 else
 {
-    Write-Host "Application Not Running"
+	Write-Host "Application Not Running"
 }
 
 if(Test-Path $DeployPath)
 {
-    Write-Host "Removing Directory Found at $DeployPath"
-    Remove-Item $DeployPath -Force -Recurse
-    Start-Sleep -s 2
+	Write-Host "Removing Directory Found at $DeployPath"
+	Remove-Item $DeployPath -Force -Recurse
+	Start-Sleep -s 2
 }
 
 Write-Host "Creating Directory at $DeployPath"
 New-Item $DeployPath -Type container
 
-Write-Host "Deploying from Content\$FrameworkNeeded to $DeployPath"
+Write-Host "Deploying from framework version $FrameworkNeeded to $DeployPath"
 Copy-Item "$scriptDir\Content\$FrameworkNeeded\*" $DeployPath -Force -Recurse
